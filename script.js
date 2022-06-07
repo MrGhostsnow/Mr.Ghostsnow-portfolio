38dc4ed030fd34268cdb24ea5ea8985ce5ee4e60
unknown <victorio5054@gmail.com>
2022-06-06 23:53:17 -0300

    criação do modal

---
+++ script.js
@@ -0,0 +1,51 @@
+const linkedin = document.getElementById("linkedin");
+const git = document.getElementById("git");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const modal = document.querySelector(".modal_contato");
+const close = document.querySelector(".close_modal");
+
+
+function abrirModal(){
+    addEventListener("click", () => {
+        modal.style.display = "flex";
+    })
+};
+
+function fecharModal(){
+    addEventListener("click", () => {
+        modal.style.display = "none";
+    })
+};
+
+
+// window.addEventListener("click", () => {
+//     if(event.target.classList.contains("#linkedin"))
+//     console.log(event)
+// })
+
+// function alterarTextoLinkedin() {
+//   addEventListener("click", () => {
+//     linkedin.innerHTML +=
+//       "<figcaption> <a href='https://www.linkedin.com/in/victor-silva-teixeira-b9a4391a3/'>Linkedin</a></figcaption>";
+//   });
+// }
+
+// function alterarTextoGit() {
+//   addEventListener("click", () => {
+//     git.innerHTML +=
+//     "<figcaption> <a href='https://www.linkedin.com/in/victor-silva-teixeira-b9a4391a3/'>Linkedin</a></figcaption>";
+// });
+// }
+
+// function alterarTextoEmail() {
+//   addEventListener("click", () => {
+//     git.innerHTML += "<figcaption>victorio5054@gmail.com</figcaption>";
+//   });
+// }
+
+// function alterarTextoPhone() {
+//   addEventListener("click", () => {
+//     git.innerHTML += "<figcaption>(77)98838-3289</figcaption>";
+//   });
+// } FALTA MAIS CONHECIMENTO
